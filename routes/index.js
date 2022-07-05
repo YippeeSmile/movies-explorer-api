c4cd1109ac3c43b50d12b54298c14959e2d9cd09
Yuliya Padzheva <yippee.smile@yandex.ru>
2022-07-05 12:14:20 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const users = require('./users');
+const movies = require('./movies');
 const NotFoundError = require('../errors/NotFoundError');
 
-router.use(require('./movies'));
-router.use(require('./users'));
+router.use(require('/users', users));
+router.use(require('/movies', movies));
 
 router.use('*', (_, __, next) => next(new NotFoundError('Запрашиваемая страница не найдена')));
 
